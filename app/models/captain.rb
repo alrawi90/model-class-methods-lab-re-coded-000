2af4fc8d7de1f9f525d0679a5c1c53d679155615
Ali Alrawi <ali.developer90@gmail.com>
2016-12-31 14:53:25 +0300

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,24 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  has_many :boat_classifications, through: :boats
+  has_many :classifications, through: :boat_classifications
+  def self.catamaran_operators
+  	Captain.distinct("captains.id").joins(:classifications).where("classifications.name = ?","Catamaran")
+  end
+  def self.sailors
+  	  Captain.distinct("captains.id").joins(:classifications).where("classifications.name = ?","Sailboat")
+  end
+    def self.motorboaters
+  	  Captain.distinct("captains.id").joins(:classifications).where("classifications.name = ?","Motorboat")
+  end
+  def self.talented_seamen
+     target_ids= self.sailors & self.motorboaters
+  	Captain.where("id in (?)", target_ids)
+  end
+  def self.non_sailors
+      target_ids= self.sailors.pluck(:id) 
+  	  Captain.where("id not in (?)", target_ids)
+  end
 end
+
+
